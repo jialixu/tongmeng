0bf2276206a40b04bdb22ee08cc55cecb899f152
jialixu <522242439@qq.com>
2016-01-13 19:14:01 +0800

    新工程

---
+++ tongmeng/tongmeng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tongmeng
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 海风集团有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tongmeng/tongmeng/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tongmeng
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 海风集团有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
